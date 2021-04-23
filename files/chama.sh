39b2258bae85374aa47986cd02a8d3ee42c1172d
Tiago <tiago.ribeiro@serpro.gov.br>
2021-04-23 15:49:38 -0300

    corrigido caminho do app.bundle e add chama.sh

---
+++ files/chama.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#Copiar arquivo de tradução da home
+cp app.bundle.min.js /usr/share/jitsi-meet/libs/
+
+#Correções em arquivos html fora do lugar
+cd /usr/share/jitsi-meet/static
+ln -s ../head.html head.html
+ln -s ../base.html base.html
+ln -s ../title.html title.html
